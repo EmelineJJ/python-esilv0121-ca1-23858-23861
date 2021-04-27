945fa6b0e2c705ed7aded65d8abff08c35b889c2
Emeline <23858@student.dorset-college.ie>
2021-04-27 17:43:39 +0200

    ht

---
+++ app.py
@@ -0,0 +1,27 @@
+# Francoise RUCH - 23861
+# Emeline JJ - 23858
+
+from flask import Flask, render_template,url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+'''
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+'''
+# A comprendre/verifier
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- readme.txt
+++ readme.txt
@@ -4,3 +4,8 @@ We tried to create .txt files and to updates codes.
 We start the code to creat the pin for customer (creatpin and dictionnary)
 
 27/04/2021 :
+We create the folder templates with two html to start our application.
+
+
+link:
+https://developer.mozilla.org/fr/docs/Web/HTML/Element/Input
