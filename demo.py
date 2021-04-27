4ee7876014b16c2747736c87cce31a663e2aa620
Emeline <23858@student.dorset-college.ie>
2021-04-27 16:02:23 +0200

    new

--- demo.py
+++ demo.py
@@ -8,3 +8,15 @@ for letter in word:
     print(letter)
 
 word = input("Enter a word: ")
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
