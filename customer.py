186474a8c80692fe9a9d06a0f4f73beb290b7274
Francoise <23861@student.dorset-college.ie>
2021-04-28 18:45:20 +0200

    update

--- customer.py
+++ customer.py
@@ -1,5 +1,8 @@
 # Emeline JJ - 23858
 # Francoise RUCH - 23861
+
+from datetime import date
+
 class Customer:
     def __init__(self,firstname,lastname,email,pin):
         self.firstname=firstname
@@ -9,6 +12,7 @@ class Customer:
    
     def dictionary(letter):
         chaine = 'abcdefghijklmnopqrstuvwxyz'
+        
         return(1+chaine.index(letter))
 
     
@@ -24,6 +28,7 @@ class Customer:
         initialPositionFirst=dictionary(firstname[0].lower())
         initialPositionLast= dictionary(lastname[0].lower())
         pin=((firstname[0]+lastname[0])+"-"+lenTotal+"-"+initialPositionFirst+"-"+initialPositionLast).lower()
+        
         return pin
     
 
@@ -33,5 +38,14 @@ class Customer:
 
     print(createPin('Joe', 'Smith'))
     
+
+    def transaction(firstname,lastname,value):
+        date=datetime.datetime.today().strftime('%Y-%m-%d')
+        if value <0:
+            typeTransaction='withdraw'
+        else:
+            typeTransaction='lodgement'
+
+        return 'fait'
       
     
--- readme.txt
+++ readme.txt
@@ -10,5 +10,20 @@ We create the folder templates with two html to start our application.
 link:
 https://developer.mozilla.org/fr/docs/Web/HTML/Element/Input
 
-28/04/2021 : 
+28/04/2021: 
 We continue to work on HTML and function to create a PIN.
+
+for 29/04/2021:
+question of problem dictionnary is not defined
+if and else apply to a button 
+look at the UML diagrams 
+do we use database for customers or txt is enough
+
+
+
+to do list:
+txt for customers 
+open and create and read
+class user abstract, inheritances
+private or public functions
+test module
