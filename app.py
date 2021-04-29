d1c665a6b9c5704857e283cf3324e796fdce026a
Emeline <23858@student.dorset-college.ie>
2021-04-29 17:11:19 +0200

    pass

--- app.py
+++ app.py
@@ -3,10 +3,7 @@
 
 from flask import Flask, render_template,url_for
 
-
 app = Flask(__name__)
-
-
 
 @app.route("/", methods=["GET","POST"])
 def home():
@@ -23,7 +20,10 @@ def customer():
 
 @app.route('/employee/app')
 def appEmployee():
-    return render_template("appEmployee.html")
+    if password == id :
+        return render_template("appEmployee.html")
+    else:
+        return "Try again !"
 
 if __name__ == "__main__":
     app.run(debug=True)
