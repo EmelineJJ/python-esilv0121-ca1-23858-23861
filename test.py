b781f8197c6277ca5e01c2f45a719bde16fb77b9
Francoise <23861@student.dorset-college.ie>
2021-05-10 13:04:42 +0200

    test

--- test.py
+++ test.py
@@ -8,22 +8,25 @@ from bank import createAccountNumber
 from bank import dictionary
 from bank import Customer
 
-
+#test class
 class UneClasseDeTest(unittest.TestCase):
 
     def setUp(self):
-        print("Avant le test")
+        print("Before")
 
     def tearDown(self):
-        print("Après le test")
+        print("After")
 
+    #test for create code Pin
     def test_1(self):
         self.assertEqual(createPin('Joe', 'Smith'), str(1019) )
     
+    #test for create account number
     def test_2(self):
         cust= Customer('1019','Joe','Smith','j@gmail.com')
         self.assertEqual(createAccountNumber(cust), 'js-8-10-19')
 
+    #test of the index of a letter in a dictionnary
     def test_3(self):
         self.assertEqual(dictionary('u'), 21)
 
